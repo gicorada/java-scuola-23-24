74bade82e26c2b9b48a83da03b42a0d4759c555f
Giacomo Radaelli <gradaellig@protonmail.com>
2023-10-11 18:26:10 +0200

    Aggiunto es 6 cap 10 Filter Measurer

---
+++ eserciziario/cap10/es6/BankAccount.java
@@ -0,0 +1,11 @@
+package eserciziario.cap10.es6;
+
+public class BankAccount {
+    private final double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() { return balance; }
+}
