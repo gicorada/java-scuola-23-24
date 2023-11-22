654219f284270f823c899cf3d27db55fec590232
Giacomo Radaelli <gradaellig@protonmail.com>
2023-11-22 16:08:21 +0100

    Aggiunto es E12.1 (area rettangolo con ricorsione)

---
+++ libro/cap12/Rectangle.java
@@ -0,0 +1,17 @@
+package libro.cap12;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        if(width == 1) return height;
+        Rectangle small = new Rectangle(width - 1, height);
+        return small.getArea() + height;
+    }
+}
---
+++ libro/cap12/Tester.java
@@ -0,0 +1,8 @@
+package libro.cap12;
+
+public class Tester {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(10, 20);
+        System.out.println(r.getArea());
+    }
+}
